4504d361843d1dc256d67d84dd2b8fb4ca2cb067
joaobatistaleoncampos <instrutorjoaoleon@gmail.com>
2025-06-29 09:33:39 -0300

    feito mudanças css.html e script

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-
-// //Função para pegar o dado
-// /**
-//  * Função para salvar os dados digitados na input
-//  * para o localStorage
-//  */
-// function saveDataToLocalStorage() {
-//     const tarefa = document.querySelector("#tarefa").value;
-//     localStorage.setItem("arquivo1", tarefa);
-//     console.log(tarefa);
-//   }
-
-
-
-
-
-
-
-//   /**
-//    * Função para exibir o dado salvo no localStorage
-//    */
-//   function showLocalStorageData() {
-//     alert("O valor guardado é: " + localStorage.getItem("arquivo1"));
-//   }
-
-//   /**
-//    * Função para limpar os dados salvos no localStorage
-//    */
-//   function clearLocalStorage() {
-//     localStorage.removeItem("arquivo1");
-//   }
-
-
-
-
 function meuEscopo() {
     const form = document.querySelector('.form');
     const resultado = document.querySelector('.resultado');
